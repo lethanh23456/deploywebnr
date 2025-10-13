418d77700ec1e89101178bfa5ee6807913db0a2a
lethanh23456 <thanhld2006.ptit@gmail.com>
2025-10-13 22:20:38 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import AllRoute from './components/AllRoute';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [isTooSmall, setIsTooSmall] = useState(window.innerWidth < 780);
+  // const [isTooSmall, setIsTooSmall] = useState(window.innerWidth < 780);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsTooSmall(window.innerWidth < 1440);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsTooSmall(window.innerWidth < 1440);
+  //   };
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
 
-  if (isTooSmall) {
-    return (
-      <div style={{ padding: '2rem', textAlign: 'center', fontSize: '1.5rem' }}>
-        Giao diện đang được cập nhật. Vui lòng sử dụng máy tính (≥780px) để truy cập trang web.
-      </div>
-    );
-  }
+  // if (isTooSmall) {
+  //   return (
+  //     <div style={{ padding: '2rem', textAlign: 'center', fontSize: '1.5rem' }}>
+  //       Giao diện đang được cập nhật. Vui lòng sử dụng máy tính (≥780px) để truy cập trang web.
+  //     </div>
+  //   );
+  // }
 
   return <AllRoute />;
 }
